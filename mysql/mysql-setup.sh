8e2a8e627734e7027bbacb6d37efb0e0fc9f60d7
Andiry Xu <jix024@cs.ucsd.edu>
2017-04-16 15:26:31 -0700

    Update mysql setup

--- mysql/mysql-setup.sh
+++ mysql/mysql-setup.sh
@@ -5,8 +5,12 @@ umount /mnt/ramdisk
 umount /mnt/scratch
 umount /data1/mysql
 rmmod nova
-insmod nova.ko measure_timing=0 inplace_data_updates=1 replica_metadata=1 metadata_csum=1 unsafe_metadata=1 \
-			wprotect=0 mmap_cow=1 data_csum=1 data_parity=1 dram_struct_csum=1
+insmod nova.ko measure_timing=0 \
+	inplace_data_updates=0 \
+	wprotect=0 mmap_cow=1 \
+	unsafe_metadata=1 \
+	replica_metadata=1 metadata_csum=1 dram_struct_csum=1 \
+	data_csum=1 data_parity=1
 
 sleep 1
 
