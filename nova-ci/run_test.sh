6205525147384a1ab7bee6214b71f85eff394b5c
Steven Swanson <swanson@cs.ucsd.edu>
2017-06-16 15:17:44 -0700

    Useful functions

--- nova-ci/run_test.sh
+++ nova-ci/run_test.sh
@@ -13,6 +13,13 @@ function get_kernel_version() {
 
 }
 
+function compute_grub_default() {
+    KERNEL_VERSION=$(get_kernel_version)
+    menu=grep 'menuentry ' /boot/grub/grub.cfg  | grep -n $KERNEL_VERSION-$K_SUFFIX | grep -v recovery | cut -f 1 -d :
+    menu=$[n-2]
+    echo "1>$menu"
+}
+
 function get_packages() {
     sudo apt-get -y update
     sudo apt-get -y build-dep linux-image-$(uname -r) fakeroot
