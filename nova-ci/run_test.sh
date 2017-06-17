4084108c2e68d72e0f867d9ce4f6a4486468a701
Steven Swanson <swanson@cs.ucsd.edu>
2017-06-16 21:52:20 -0700

    better logging

--- nova-ci/run_test.sh
+++ nova-ci/run_test.sh
@@ -1,16 +1,22 @@
  #!/usr/bin/env bash
 
+if ! [ -f run_test.sh ]; then
+    echo You are running in the wrong directory.
+    exit 1
+fi
+
 DATE=$(date +"%F-%H-%M-%S.%N")
 R=$PWD/results/$DATE
 mkdir -p $R
+ln -sf $PWD/results/latest $R
 CI_HOME=$HOME/nova-testscripts/nova-ci/
 K_SUFFIX=nova
 
 function get_kernel_version() {
-    (cd $CI_HOME/linux-nova; 
+    (
+	cd $CI_HOME/linux-nova; 
 	make kernelversion
 	)
-
 }
 
 function compute_grub_default() {
@@ -35,10 +41,12 @@ function update_kernel () {
 function build_kernel () {
     pushd $CI_HOME
     cp ../kernel/gce.config ./linux-nova/.config
-    (set -v;
+    sudo rm -rf *.tar.gz *.dsc *.deb *.changes
+    (
+	set -v;
 	cd linux-nova; 
-	make  deb-pkg LOCALVERSION=-${K_SUFFIX}
-	) > $R/kernel_build.log
+	make  deb-pkg LOCALVERSION=-${K_SUFFIX};
+	) 2>&1 | tee $R/kernel_build.log 
     popd
     KERNEL_VERSION=$(get_kernel_version)
 }
@@ -46,15 +54,18 @@ function build_kernel () {
 function install_kernel() {
     KERNEL_VERSION=$(get_kernel_version)
     pushd $CI_HOME
-    (cd $CI_HOME;
+    (
+	set -v;
+	cd $CI_HOME;
 	sudo dpkg -i   linux-image-${KERNEL_VERSION}-${K_SUFFIX}_${KERNEL_VERSION}-${K_SUFFIX}-?_amd64.deb &&
-	sudo dpkg -i linux-headers-${KERNEL_VERSION}-${K_SUFFIX}_${KERNEL_VERSION}-${K_SUFFIX}-?_amd64.deb) || false
+	sudo dpkg -i linux-headers-${KERNEL_VERSION}-${K_SUFFIX}_${KERNEL_VERSION}-${K_SUFFIX}-?_amd64.deb
+	) || false
     sudo update-grub
 }
 
 function reboot_to_nova() {
-    echo Rebooting...
-    sudo grub-reboot $(compute_grub_default )
+    echo Rebooting to $(compute_grub_default)...
+    sudo grub-reboot $(compute_grub_default)
     sudo systemctl reboot -i
 }
 
@@ -80,6 +91,10 @@ function update_and_build_nova() {
 	
 	if git diff --name-only origin/master | grep -v fs/nova || 
 	    ! [ -f /boot/vmlinuz-${KERNEL_VERSION}-* ]; then
+	    echo Main kernel is out of date or missing
+	    git diff --name-only origin/master
+	    ls /boot/*
+	    
 	    git pull
 	    build_kernel
 	    if install_kernel; then
@@ -92,6 +107,8 @@ function update_and_build_nova() {
 	    build_module
 	fi
     else
+	echo Linux sources missing
+
 	git clone git@github.com:NVSL/linux-nova.git
 	cd linux-nova
 	build_kernel
@@ -104,6 +121,12 @@ function update_and_build_nova() {
     popd 
 }
 
+function mount_nova() {
+    
+}
 
-get_packages
-update_and_build_nova
+(
+    set -v
+    get_packages
+    update_and_build_nova
+) 2>&1 | tee  $R/run_test.log
