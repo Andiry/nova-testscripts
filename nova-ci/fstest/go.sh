7b3800ad881201844b029f9837a6011f06f75b75
Steven Swanson <swanson@cs.ucsd.edu>
2017-07-13 13:16:06 -0700

    support for POSIX fstest

---
+++ nova-ci/fstest/go.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash -v
+
+. $NOVA_CI_HOME/test_util.sh
+
+export FSTEST=$(clone_or_pull git@github.com:NVSL/pjd-fstest.git)
+
+set -v
+
+cd $FSTEST
+
+if [ ".yes" == ".$REBUILD" ]; then
+    make
+fi
+
+pwd
+P=$PWD
+cd $NOVA_CI_PRIMARY_FS
+sudo prove -rv $* $P | tee ${NOVA_CI_LOG_DIR}/fstest-results.out
+#sudo ./to_junit.py < ${NOVA_CI_LOG_DIR}/xfstests-results.out > ${NOVA_CI_LOG_DIR}/xfstests-results.xml
+
+
+
