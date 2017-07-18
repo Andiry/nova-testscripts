ac83e6dbb8cea3b19021244f112f78f00bc78865
Steven Swanson <swanson@cs.ucsd.edu>
2017-07-17 21:13:48 -0700

    actually run ltp

--- nova-ci/ltp/go.sh
+++ nova-ci/ltp/go.sh
@@ -16,7 +16,7 @@ else
     sudo cp ./runltp /opt/ltp/runltp
     sudo cp ./runtest/* /opt/ltp/runtest/
 fi
-sudo /opt/ltp/runltp -f nova-empty -d $NOVA_CI_PRIMARY_FS $* 2>&1 | tee ${NOVA_CI_LOG_DIR}/nova-ltp-results.out
+sudo /opt/ltp/runltp -f nova -d $NOVA_CI_PRIMARY_FS $* 2>&1 | tee ${NOVA_CI_LOG_DIR}/nova-ltp-results.out
 #./to_junit.py < ${NOVA_CI_LOG_DIR}/nova-ltp-results.out > ${NOVA_CI_LOG_DIR}/nova-ltp-results.xml
 
 
