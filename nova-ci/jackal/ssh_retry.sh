0773266da0663fd73458433fa721a72628d73ac9
Steven Swanson <swanson@cs.ucsd.edu>
2017-08-05 21:54:14 +0000

    Handle reused IPs

--- nova-ci/jackal/ssh_retry.sh
+++ nova-ci/jackal/ssh_retry.sh
@@ -1,8 +1,16 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 host=$1
 shift
-while ! ssh $host true; do
+
+# clear out known_hosts.  Otherwise, when google recycles IPs, we get failures
+# and ssh becomes interactive.
+touch ~/.ssh/known_hosts
+cp ~/.ssh/known_hosts ~/.ssh/known_hosts.bak
+grep -v $host < ~/.ssh/known_hosts.bak > ~/.ssh/known_hosts
+
+# automatically connect to unknown hosts, and don't hash so the grep above works.
+while ! ssh -o StrictHostKeyChecking=no -o HashKnownHosts=no $host true; do
     echo Retrying $1 
     sleep 1;
 done
