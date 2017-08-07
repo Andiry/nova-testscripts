bdf37f3b1ff48580acb16f6686f37fcb5c51c740
Steven Swanson <swanson@cs.ucsd.edu>
2017-08-07 22:17:25 +0000

    set correct prefix

--- nova-ci/jackal/jenkins_run.sh
+++ nova-ci/jackal/jenkins_run.sh
@@ -2,4 +2,5 @@
 
 ./prepare_env.sh
 . venv/bin/activate
-./run_tests.py --runner gce -v --tests xfstests1 --configs baseline --instance_prefix test --configs all --tests xfstests-all
+#./run_tests.py --runner gce -v --tests xfstests1 --configs baseline --instance_prefix test --configs all --tests xfstests-all
+./run_tests.py --runner gce -v --tests xfstests1 --configs baseline --instance_prefix jenkins --configs baseline-0-0-0-1-0-0 baseline-0-0-0-0-0-0 baseline-1-1-1-0-1-1 --tests xfstests-all
