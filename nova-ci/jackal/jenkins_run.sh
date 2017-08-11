1bf31756e144bac56d0ce0d5a64121993848fd7c
Steven Swanson <swanson@cs.ucsd.edu>
2017-08-11 03:28:45 +0000

    unified jenkins run script

--- nova-ci/jackal/jenkins_run.sh
+++ nova-ci/jackal/jenkins_run.sh
@@ -2,5 +2,12 @@
 
 ./prepare_env.sh
 . venv/bin/activate
-#./run_tests.py --runner gce -v --tests xfstests1 --configs baseline --instance_prefix test --configs all --tests xfstests-all
-./run_tests.py --runner gce  --tests xfstests1 --configs baseline --instance_prefix jenkins --configs baseline-0-0-0-1-0-0 baseline-0-0-0-0-0-0 baseline-1-1-1-0-1-1 --tests xfstests-all
+
+case "$1" in
+    core)
+	args="--configs baseline --instance_prefix jenkins --configs baseline-0-0-0-1-0-0 baseline-0-0-0-0-0-0 baseline-1-1-1-0-1-1 --tests xfstests-all";;
+    test)
+	args="-v --tests xfstests1 --configs baseline --dont_kill_runner --instance_prefix jenkins-test --reuse_image";;
+esac
+
+echo ./run_tests.py --runner gce  --dont_double_expect  $args
