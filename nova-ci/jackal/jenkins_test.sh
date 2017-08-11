7fc7a99b2f828864758a6c13106b9baaa9a129d0
Steven Swanson <swanson@cs.ucsd.edu>
2017-08-11 03:08:58 +0000

    use an existing configuration

--- nova-ci/jackal/jenkins_test.sh
+++ nova-ci/jackal/jenkins_test.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+
 ./prepare_env.sh
 . venv/bin/activate
-./run_tests.py --runner gce -v --tests xfstests1 --configs baseline1 --dont_kill_runner --instance_prefix jenkins-test --reuse_image
+
+set -v
+
+./run_tests.py --runner gce -v --tests xfstests1 --configs baseline --dont_kill_runner --instance_prefix jenkins-test --reuse_image
