7cabd6dd22cedefb2454d197b52709b1cbdf4211
Steven Swanson <swanson@cs.ucsd.edu>
2017-08-13 02:47:01 +0000

    deal with python correctly

--- nova-ci/jackal/prepare_env.sh
+++ nova-ci/jackal/prepare_env.sh
@@ -3,7 +3,6 @@
 gcloud config set compute/zone us-west1-c
 gcloud config set compute/region us-west1
 
-sudo apt-get install virtualenv pip
 virtualenv venv
 . venv/bin/activate
 pip install pexpect
