0e8da159486aaf2374851865b44cba083af922e2
Steven Swanson <swanson@cs.ucsd.edu>
2017-08-13 03:18:10 +0000

    allow additional arguments on command line

--- nova-ci/jackal/jenkins_run.sh
+++ nova-ci/jackal/jenkins_run.sh
@@ -3,11 +3,14 @@
 ./prepare_env.sh
 . venv/bin/activate
 
-case "$1" in
+mode=$1
+case "$mode" in
     core)
-	args="--configs baseline --instance_prefix jenkins --configs baseline-0-0-0-1-0-0 baseline-0-0-0-0-0-0 baseline-1-1-1-0-1-1 --tests xfstests-all --dont_kill_runner --reuse_image";;
+	args="--configs baseline --instance_prefix jenkins --configs baseline-0-0-0-1-0-0 baseline-0-0-0-0-0-0 baseline-1-1-1-0-1-1 --tests xfstests-all --dont_kill_runner --reuse_image"
+	shift;;
     test)
-	args="-v --tests xfstests1 --configs baseline --dont_kill_runner --instance_prefix jenkins-test --reuse_image";;
+	args="-v --tests xfstests1 --configs baseline --dont_kill_runner --instance_prefix jenkins-test --reuse_image"
+	shift;;
 esac
 
-./run_tests.py --runner gce  --dont_double_expect  $args
+./run_tests.py --runner gce  --dont_double_expect  $args $*
