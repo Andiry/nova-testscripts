25d8c2ea4af2c31760ba282b4b761e0628be55d2
Steven Swanson <swanson@cs.ucsd.edu>
2017-08-13 21:55:45 +0000

    set prefix based on git hash

--- nova-ci/test_util.sh
+++ nova-ci/test_util.sh
@@ -4,19 +4,23 @@ function clone_or_pull() {
     repo=$1
     dir=$(echo $repo | perl -ne '/github.com(:|\/)[\w-]+\/([-\w]+)/; print $2')
 
-    if [ -d $dir ]; then
-	(
-	    cd $dir
-	    git fetch 1>&2
-	    git pull 1>&2
-	)
-    else
+    if ! [ -d $dir ]; then
 	git clone $repo  1>&2
     fi
+    (
+	cd $dir
+	for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master `; do
+	    git branch --track ${branch#remotes/origin/} $branch
+	done;
+	git fetch --all 1>&2
+	git pull 1>&2
+    )
 
     if [ ".$2" != "." ]; then
-	cd $dir
-	git checkout -b $2 1>&2
+	(
+	    cd $dir
+	    git checkout $2 1>&2
+	)
     fi
     
     echo $dir
