15362d1a887f34d408d97748d8478dc9baf09110
Steven Swanson <swanson@cs.ucsd.edu>
2017-08-14 03:41:44 +0000

    hide git output

--- nova-ci/test_util.sh
+++ nova-ci/test_util.sh
@@ -4,6 +4,9 @@ function clone_or_pull() {
     repo=$1
     dir=$(echo $repo | perl -ne '/github.com(:|\/)[\w-]+\/([-\w]+)/; print $2')
 
+    echo repo=$repo 1>&2
+    echo dir=$dir 1>&2
+    
     if ! [ -d $dir ]; then
 	git clone $repo  1>&2
     fi
@@ -12,9 +15,9 @@ function clone_or_pull() {
 	for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master `; do
 	    git branch --track ${branch#remotes/origin/} $branch
 	done;
-	git fetch --all 1>&2
-	git pull 1>&2
-    )
+	git fetch --all 
+	git pull 
+    ) 1>&2
 
     if [ ".$2" != "." ]; then
 	(
